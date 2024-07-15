6d887c3352f840c528c617fd72f0afb74b14fd9a
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2024-07-15 09:05:40 +0530

    Add files via upload
    Day - 01 - Of SQL Course

---
+++ Day1_SQL/Day1_Practice2.sql
@@ -0,0 +1,83 @@
+# Question 1:
+# 1) Create a Database Bank.
+
+# Question 2:
+# 2) Create a table with the name “Bank_Inventory” with the following columns
+-- Product  with string data type and size 10 --
+-- Quantity with numerical data type --
+-- Price with data type that can handle all real numbers
+-- purcahase_cost with data type which always shows two decimal values --
+-- estimated_sale_price with data type float --
+
+# Question 3:
+# 3) Display all column names and their datatype and size in Bank_Inventory.
+
+# Question 4:
+# 4) Insert the below two records into Bank_Inventory table .
+-- 1st record with values --
+			-- Product : PayCard
+			-- Quantity: 2 
+			-- price : 300.45 
+			-- Puchase_cost : 8000.87
+			-- estimated_sale_price: 9000.56 --
+-- 2nd record with values --
+			-- Product : PayPoints
+			-- Quantity: 4
+			-- price : 200.89 
+			-- Puchase_cost : 7000.67
+			-- estimated_sale_price: 6700.56
+
+# Question 5:
+# 5) Add a column : Geo_Location to the existing Bank_Inventory table with data type varchar and size 20 .
+
+# Question 6:
+# 6) What is the value of Geo_Location for product : ‘PayCard’?
+
+# Question 7:
+# 7) How many characters does the  Product : ‘PayCard’ store in the Bank_Inventory table.
+
+# Question 8:
+# a) Update the Geo_Location field from NULL to ‘Delhi-City’ 
+# b) How many characters does the  Geo_Location field value ‘Delhi-City’ stores in the Bank_Inventory table
+
+# Question 9:
+# 9) update the Product field from CHAR to VARCHAR size 10 in Bank_Inventory 
+
+# Question 10:
+# 10) Reduce the size of the Product field from 10 to 6 and check if it is possible. 
+
+# Question 11:
+# 11) Bank_inventory table consists of ‘PayCard’ product details .
+-- For ‘PayCard’ product, Update the quantity from 2 to 10  
+
+ # Question 12:
+# 12) Create a table named as Bank_Holidays with below fields 
+-- a) Holiday field which displays or accepts only date 
+-- b) Start_time field which also displays or accepts date and time both.  
+-- c) End_time field which also displays or accepts date and time along with the timezone also. 
+
+ # Question 13:
+# 13) Step 1: Insert today’s date details in all fields of Bank_Holidays 
+-- Step 2: After step1, perform the below 
+-- Postpone Holiday to next day by updating the Holiday field 
+
+# Question 14:
+# 14) Modify  the Start_time data with today's datetime in the Bank_Holidays table 
+
+# Question 15:
+# 15) Update the End_time with UTC time(GMT Time) in the Bank_Holidays table. 
+
+# Question 16:
+# 16) Select all columns from Bank_Inventory without mentioning any column name
+
+# Question 17:
+# 17)  Display output of PRODUCT field as NEW_PRODUCT in  Bank_Inventory table 
+
+# Question 18:
+# 18)  Display only one record from bank_Inventory 
+
+# Question 19:
+# 19) Modify  the End_time data with today's datetime in the Bank_Holidays table 
+
+# Question 20:
+# 20) Display the first five rowss of the Geo_location field of Bank_Inventory.
