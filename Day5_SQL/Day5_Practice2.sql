1878218f3462b8dd22298468d3a2997c682c8a12
UDAY RAMA KOTESWA RAO NALABOTHU <111573702+Nurkrao2002@users.noreply.github.com>
2024-09-30 21:39:58 +0530

    Add files via upload

---
+++ Day5_SQL/Day5_Practice2.sql
@@ -0,0 +1,37 @@
+# Dataset used: titanic_ds.csv
+# Use subqueries for every question
+
+#Q1. Display the first_name, last_name, passenger_no , fare of the passenger who paid less than the  maximum fare. (20 Row)
+
+#Q2. Retrieve the first_name, last_name and fare details of those passengers who paid fare greater than average fare. (11 Rows)
+
+#Q3. Display the first_name ,sex, age, fare and deck_number of the passenger equals to passenger number 7 but exclude passenger number 7.(3 Rows)
+ 
+#Q4. Display first_name,embark_town where deck is equals to the deck of embark town ends with word 'town' (7 Rows)
+
+# Dataset used: youtube_11.csv
+
+#Q5. Display the video Id and the number of likes of the video that has got less likes than maximum likes(10 Rows)
+
+#Q6. Write a query to print video_id and channel_title where trending_date is equals to the trending_date of  category_id 1(5 Rows)
+
+#Q7. Write a query to display the publish date, trending date ,views and description where views are more than views of Channel 'vox'.(7 Rows))
+
+#Q8. Write a query to display the channel_title, publish_date and the trending_date having category id in between 9 to Maximum category id .
+# Do not use Max function(3 Rows)
+
+#Q9. Write a query to display channel_title, video_id and number of view of the video that has received more than  mininum views. (10 Rows)
+
+# Database used: db1 (db1.sql file provided)
+
+#Q10. Get those order details whose amount is greater than 100,000 and got cancelled(1 Row))
+
+#Q11. Get employee details who shipped an order within a time span of two days from order date (15 Rows)
+
+#Q12. Get product name , product line , product vendor of products that got cancelled(53 Rows)
+ 
+#Q13. Get customer full name along with phone number ,address , state, country, who's order was resolved(4 Rows)
+ 
+#Q14. Display those customers who ordered product of price greater than average price of all products(98 Rows)
+ 
+#Q15. Get office deatils of employees who work in the same city where their customers reside(5 Rows)
